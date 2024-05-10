50b4a5d6e0f0114c330c1e6138dca59ff013657c
Fangji Qian <fangji.qian@student.uts.edu.au>
2024-05-10 19:27:43 +1000

    Initial Commit

---
+++ IOS Assignment 3/IOS_Assignment_3App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Assignment_3App.swift
+//  IOS Assignment 3
+//
+//  Created by admin on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Assignment_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
